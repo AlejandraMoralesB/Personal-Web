757cdee7adac82d57a1f461fc8b376f386833a36
AlejandraMoralesB <a.moralesbarra@gmail.com>
2019-10-25 22:32:11 -0300

    modificacion de proyecto , forma y links

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const event2 = document.getElementById("secondEvent");
 const event3 = document.getElementById("thirdEvent");
 const event4 = document.getElementById("fourthEvent");
 
-
-event1.addEventListener("click", () => window.open("https://zuriros.github.io/lim-2018-11-bc-core-am-cipher/src", "_blank"));
-event2.addEventListener("click", () => window.open("https://zuriros.github.io/lim-2018-11-bc-core-am-data-lovers/src", "_blank"));
-event3.addEventListener("click", () => window.open("https://zuriros.github.io/LIM008-social-network/src/", "_blank"));
-event4.addEventListener("click", () => window.open("https://github.com/zuriros/md-links", "_blank"));
+event1.addEventListener("click", () =>
+  window.open(
+    "https://alejandramoralesb.github.io/SCL010-data-lovers/src/index.html",
+    "_blank"
+  )
+);
+event2.addEventListener("click", () =>
+  window.open(
+    "https://github.com/AlejandraMoralesB/SCL010-Burger-Queen",
+    "_blank"
+  )
+);
+event3.addEventListener("click", () =>
+  window.open(
+    "https://carolgmonteiro.github.io/SCL010-hackathon-peliculas/src/index.html",
+    "_blank"
+  )
+);
+event4.addEventListener("click", () =>
+  window.open("https://github.com/AlejandraMoralesB/SCL010-md-links", "_blank")
+);
